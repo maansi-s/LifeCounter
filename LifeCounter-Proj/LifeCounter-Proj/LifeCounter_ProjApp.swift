3820236bb65541cb591e4087c2b65e94b13a402a
msurve2@uw.edu <msurve2@uw.edu>
2025-04-29 16:35:08 -0700

    Added proj

---
+++ LifeCounter-Proj/LifeCounter-Proj/LifeCounter_ProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCounter_ProjApp.swift
+//  LifeCounter-Proj
+//
+//  Created by Maansi Surve on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct LifeCounter_ProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LifeCounter-Proj/LifeCounter-ProjTests/LifeCounter_ProjTests.swift
@@ -0,0 +1,17 @@
+//
+//  LifeCounter_ProjTests.swift
+//  LifeCounter-ProjTests
+//
+//  Created by Maansi Surve on 4/29/25.
+//
+
+import Testing
+@testable import LifeCounter_Proj
+
+struct LifeCounter_ProjTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
